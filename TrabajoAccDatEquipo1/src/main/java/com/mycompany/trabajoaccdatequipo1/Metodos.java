3492e1f0f500c6534b481b615f32167ed0016403
Ruben Javier Bacho Alarcon <130557846+RubenJavierBachoAlarcon@users.noreply.github.com>
2024-02-06 09:36:54 +0100

    06/02/2024 deleteIa / insertIa

--- TrabajoAccDatEquipo1/src/main/java/com/mycompany/trabajoaccdatequipo1/Metodos.java
+++ TrabajoAccDatEquipo1/src/main/java/com/mycompany/trabajoaccdatequipo1/Metodos.java
@@ -20,20 +20,20 @@ import javax.persistence.TypedQuery;
  * @author pokem, bacho, lucas
  */
 public class Metodos {
-    
+
     static EntityManagerFactory emf;
     static EntityManager em;
-    
+
     /**
      * Método que establece la conexión a Oracle.
      */
     public static void establecerConexion() {
         emf = Persistence.createEntityManagerFactory("com.mycompany_TrabajoAccDatEquipo1_jar_1.0-SNAPSHOTPU");
         em = emf.createEntityManager();
-        
+
         System.out.println("Conexión establecida.");
     }
-    
+
     /**
      * Método que cierra la conexión con Oracle.
      */
@@ -41,21 +41,19 @@ public class Metodos {
         em.close();
         emf.close();
     }
-    
+
     /**
      * Métodos para insertar datos de prueba con los que poder trabajar.
      */
     public static void insertarDatos() {
         em.getTransaction().begin();
-        
-        
+
         // INSERT DE LOS TIPOS
         Tipos tipo1 = new Tipos(1, "Generación de texto por chat", "IAs con capacidad de recibir prompts en forma de texto y responder a ellas en formato chat.");
         em.persist(tipo1);
         Tipos tipo2 = new Tipos(2, "Generación de imágenes", "IAs que indexan búsquedas en internet y mezclan resultados para generar una imagen que responde a una serie de prompts proporcionados.");
         em.persist(tipo2);
-        
-        
+
         // INSERT DE LAS IAS
         //las del tipo 1
         Ias ia1 = new Ias(1, "ChatGPT", "GPT", tipo1);
@@ -71,9 +69,7 @@ public class Metodos {
         em.persist(ia5);
         Ias ia6 = new Ias(6, "Dreambooth", "Stable Diffusion", tipo2);
         em.persist(ia6);
-        
-        
-        
+
         // INSERT DE LOS PROMPTS
         //los de texto
         Prompts p1 = new Prompts(1, "Resume este texto", null);
@@ -89,9 +85,7 @@ public class Metodos {
         em.persist(p5);
         Prompts p6 = new Prompts(6, "Crea una represntación visual de la teoría de cuerdas, concepto científico abstracto.", null);
         em.persist(p6);
-        
-        
-        
+
         // INSERT DE LAS INSTANCIAS INTERMEDIAS PARA LA TABLA N:M
         // las de texto:
         //combinaciones de ia1 con p1, p2 y p3
@@ -138,18 +132,16 @@ public class Metodos {
         em.persist(ip17);
         IasPrompts ip18 = new IasPrompts(18, ia6, p6);
         em.persist(ip18);
-        
-        
+
         em.getTransaction().commit();
     }
-    
+
     /**
-     * Método que permite realizar la inserción de un tipo de ia en la base
-     * de datos.
-     * Antes de realizarse la inserción, se hace una búsqueda en la base de
-     * datos; primero por id y luego por nombre. Si el tipo de IA que se intenta
-     * insertar NO existe, se inserta.
-     * 
+     * Método que permite realizar la inserción de un tipo de ia en la base de
+     * datos. Antes de realizarse la inserción, se hace una búsqueda en la base
+     * de datos; primero por id y luego por nombre. Si el tipo de IA que se
+     * intenta insertar NO existe, se inserta.
+     *
      * @param id id del tipo a insertar
      * @param nombre nombre del tipo de IA
      * @param desc descripción de los usos para los que se emplea el tipo de ia
@@ -157,44 +149,44 @@ public class Metodos {
     public static void insertarTipo(int id, String nombre, String desc) {
         em.getTransaction().begin();
         Tipos tipoBusca = null;
-        
+
         // Primero se busca por id; si existe, mensaje de error
         tipoBusca = em.find(Tipos.class, id, LockModeType.PESSIMISTIC_READ);
         if (tipoBusca != null) {
             System.out.println("Ya existe ese tipo en la base de datos.");
         } else {
-            
+
             // Luego se busca por nombre; si existe, mensaje de error; si no, se inserta
             TypedQuery<Tipos> query = em.createQuery("select t from Tipos t where t.tipo=:NOMBREP", Tipos.class);
             query.setParameter("NOMBREP", nombre);
             try {
-                
+
                 tipoBusca = query.getSingleResult();
                 System.out.println("Ya existe un tipo con ese nombre en la base de datos.");
-                
+
             } catch (NoResultException e) {
-                
+
                 Tipos tipoInsertar = new Tipos(id, nombre, desc);
                 em.persist(tipoInsertar);
                 System.out.println("Tipo insertado con éxito.");
-                
+
             }
         }
-        
+
         em.getTransaction().commit();
     }
-    
+
     /**
      * Método para ver todos los datos existentes en la tabla "tipos de IAs".
      * Aunque la rúbrica especifica hacerlo sin JPQL, no es posible hacer una
-     * select de todos los datos sin él, por lo que el uso de una typed query
-     * es forzoso.
+     * select de todos los datos sin él, por lo que el uso de una typed query es
+     * forzoso.
      */
     public static void verTipos() {
         em.getTransaction().begin();
-        
+
         TypedQuery<Tipos> query = em.createQuery("select t from Tipos t", Tipos.class);
-        
+
         Collection<Tipos> colec = query.getResultList();
         if (colec.isEmpty()) {
             System.out.println("No existen datos en la tabla 'Tipos de IAs'.");
@@ -209,44 +201,44 @@ public class Metodos {
                 System.out.println("--------------------------------------------------");
             }
         }
-        
+
         em.getTransaction().commit();
     }
-    
+
     /**
-     * Método que localiza, SIN USAR JPQL, un tipo en base a su id. Si 
-     * el tipo no existe, muestra un mensaje de error. Una vez
-     * encontrado, muestra sus datos y los de la colección de IAs que tenga
-     * asociada.
+     * Método que localiza, SIN USAR JPQL, un tipo en base a su id. Si el tipo
+     * no existe, muestra un mensaje de error. Una vez encontrado, muestra sus
+     * datos y los de la colección de IAs que tenga asociada.
+     *
      * @param id id del tipo a encontrar
      */
     public static void verTipoDatos(int id) {
         em.getTransaction().begin();
-        
+
         Tipos tipo = null;
         tipo = em.find(Tipos.class, id, LockModeType.PESSIMISTIC_READ);
-        
-        if (tipo==null) {
+
+        if (tipo == null) {
             System.out.println("No existe el tipo con id " + id + ".");
         } else {
-            
+
             System.out.println("ID del tipo: " + tipo.getIdtipo());
             System.out.println("Tipo: " + tipo.getTipo());
             System.out.println("Descripción: " + tipo.getDescripcion());
             System.out.println("--------------------------------------------------");
             System.out.println("--------------------------------------------------");
-            
+
             Collection<Ias> colec = tipo.getIasCollection();
             if (colec.isEmpty()) {
                 System.out.println("El tipo con id " + tipo.getIdtipo() + "no tiene IAs asociadas.");
             } else {
-                
+
                 System.out.println("El tipo con id " + tipo.getIdtipo() + " dispone de IAs asociadas.");
                 System.out.println("");
                 Ias ia = null;
                 Iterator<Ias> it = colec.iterator();
                 while (it.hasNext()) {
-                    
+
                     ia = it.next();
                     System.out.println("ID de la IA: " + ia.getIdia());
                     System.out.println("Nombre de la IA: " + ia.getNombre());
@@ -254,100 +246,139 @@ public class Metodos {
                     System.out.println("Nº de veces usada: " + ia.getUsos());
                     System.out.println("Popularidad: " + ia.getPopularidad());
                     System.out.println("--------------------------------------------------");
-                    
+
                 }
             }
-            
+
         }
-        
-        
-        
+
         em.getTransaction().commit();
     }
-    
+
     /**
      * LÓGICA DE NEGOCIO: NO PUEDEN BORRARSE REGISTROS DE LOS QUE DEPENDAN OTROS
-     * PRIMERO HABRÁ QUE BORRAR ESOS OTROS REGISTROS DEPENDIENTES
-     * Método que hace el borrado de un tipo buscándolo por su ID. Primero se
-     * controla que el tipo exista. Si no existe, mensaje de error.
-     * Tras ver que exista, se controla que tenga registros que dependan de
-     * él. Si los tiene, mensaje de error.
-     * Si el tipo existe y no hay IAs que dependan de él, el borrado se hace
-     * correctamente.
+     * PRIMERO HABRÁ QUE BORRAR ESOS OTROS REGISTROS DEPENDIENTES Método que
+     * hace el borrado de un tipo buscándolo por su ID. Primero se controla que
+     * el tipo exista. Si no existe, mensaje de error. Tras ver que exista, se
+     * controla que tenga registros que dependan de él. Si los tiene, mensaje de
+     * error. Si el tipo existe y no hay IAs que dependan de él, el borrado se
+     * hace correctamente.
+     *
      * @param id id del tipo a borrar
      */
     public static void borrarTipo(int id) {
         em.getTransaction().begin();
         Tipos tipo = null;
-        
+
         // Se busca el tipo
         tipo = em.find(Tipos.class, id, LockModeType.PESSIMISTIC_READ);
-        
+
         // Si el tipo no existe, mensaje de error
-        if (tipo==null) {
+        if (tipo == null) {
             System.out.println("No existe el tipo con id " + id + ".");
         } else {
-            
+
             // Antes de hacer el borrado, se verifica que el tipo no tenga registros asociados
             // Se recoge su lista de IAs asociadas
             // Si está vacía, el registro se borra correctamente
             // Si no, mensaje de error para evitar que el usuario borre registros sin querer
             Collection<Ias> colec = tipo.getIasCollection();
-            
+
             // Si la lista está vacía
             if (colec.isEmpty()) {
                 em.remove(tipo);
                 System.out.println("El tipo con id " + id + " se ha borrado correctamente.");
-            // Si no    
+                // Si no    
             } else {
                 System.out.println("No es posible borrar ese registro.");
                 System.out.println("¡¡Existen otros registros que dependen de él!!");
                 System.out.println("Borra esos registros e inténtalo de nuevo.");
             }
-            
+
         }
-        
-        
+
         em.getTransaction().commit();
     }
-    
+
     /**
-     * Método que permite hacer la modificación de un registro "tipo de IA". Para
-     * que se pueda modificar un registro, EL USUARIO TIENE QUE ESCRIBIR EN TODOS
-     * LOS CAMPOS. SI NO ESTÁN TODOS RELLENOS, MENSAJE DE ERROR.
-     * Tras eso se busca que exista el registro a modificar. Si no existe, 
-     * mensaje de error.
-     * 
+     * Método que permite hacer la modificación de un registro "tipo de IA".
+     * Para que se pueda modificar un registro, EL USUARIO TIENE QUE ESCRIBIR EN
+     * TODOS LOS CAMPOS. SI NO ESTÁN TODOS RELLENOS, MENSAJE DE ERROR. Tras eso
+     * se busca que exista el registro a modificar. Si no existe, mensaje de
+     * error.
+     *
      * @param id id del tipo a modificar
      * @param nombre nuevo nombre para el tipo
      * @param desc nueva descripción para el tipo
      */
     public static void modificarTipo(String id, String nombre, String desc) {
         if (id.isEmpty() || id.isBlank() || nombre.isEmpty() || nombre.isBlank() || desc.isEmpty() || desc.isBlank()) {
-            
+
             System.out.println("Error en la modificación.");
             System.out.println("Se deben rellenar todos los campos.");
-            
+
         } else {
             em.getTransaction().begin();
-            
+
             int idTipo = Integer.parseInt(id);
             Tipos tipo = null;
-            
+
             tipo = em.find(Tipos.class, idTipo, LockModeType.PESSIMISTIC_READ);
             if (tipo == null) {
-                
+
                 System.out.println("No existe el tipo con id " + idTipo + ".");
-                
+
             } else {
-                
+
                 tipo.setTipo(nombre);
                 tipo.setDescripción(desc);
-                System.out.println("Se ha modificado correctamente el tipo con id"  + idTipo + " .");
-                
+                System.out.println("Se ha modificado correctamente el tipo con id" + idTipo + " .");
+
             }
-            
+
             em.getTransaction().commit();
         }
     }
+
+    public void deleteIA(int idIa) {
+        em.getTransaction().begin();
+
+        Ias ia = em.find(Ias.class, idIa);
+        em.remove(ia);
+
+        em.getTransaction().commit();
+    }
+
+    public static void insertIa(Ias iaActual, Tipos tipoActual) {
+        em.getTransaction().begin();
+        Tipos tipo = em.find(Tipos.class, tipoActual.getIdtipo());
+
+        if (tipo == null) {
+            System.out.println("El tipo no existe");
+            return;
+        }
+
+        Ias ia = em.find(Ias.class, iaActual.getIdia());
+
+        if (ia != null) {
+            System.out.println("El ID de la IA ya existe.");
+            return;
+        }
+
+        TypedQuery<Ias> query = em.createQuery("SELECT i FROM Ias i WHERE i.nombre = :nombre", Ias.class);
+        query.setParameter("nombre", iaActual.getNombre());
+        Collection<Ias> iasConMismoNombre = query.getResultList();
+        if (!iasConMismoNombre.isEmpty()) {
+            System.out.println("El nombre de la IA ya existe.");
+            return;
+        }
+        
+        
+        iaActual.setIdtipo(tipoActual);
+        em.persist(iaActual);
+        em.getTransaction().commit();
+    }
+    
+    
+
 }
--- TrabajoAccDatEquipo1/src/main/java/com/mycompany/trabajoaccdatequipo1/TrabajoAccDatEquipo1.java
+++ TrabajoAccDatEquipo1/src/main/java/com/mycompany/trabajoaccdatequipo1/TrabajoAccDatEquipo1.java
@@ -15,10 +15,24 @@ public class TrabajoAccDatEquipo1 {
         
         //Metodos.insertarDatos();
         //Metodos.insertarTipo(3, "Generación de audio y música", "Ias empleadas para generar y retransmitir audio, ya sean voces o ruidos, según las especificaciones dadas.");
-        Metodos.verTipos();
-        Metodos.verTipoDatos(2);
+        //Metodos.verTipos();
+        //Metodos.verTipoDatos(2);
         //Metodos.borrarTipo(2);
         //Metodos.modificarTipo("2", "Cambio de nombre de prueba", "Cambio de descripción de prueba");
+        
+        Ias iaActual = new Ias();
+        iaActual.setIdia(8);
+        iaActual.setNombre("IA1");
+        iaActual.setModelo("Modelo1");
+        iaActual.setUsos(0);
+        iaActual.setPopularidad("Alta");
+
+        Tipos tipoActual = new Tipos();
+        tipoActual.setIdtipo(2);
+        tipoActual.setTipo("Tipo1");
+        tipoActual.setDescripción("Descripcion1");
+        
+        Metodos.insertIa(iaActual, tipoActual);
         
         Metodos.cerrarConexion();
     }
